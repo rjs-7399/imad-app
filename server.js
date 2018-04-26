f9d9e127c1f88082dcaadc9c5460c909938fe312
rjs-7399 <38734146+rjs-7399@users.noreply.github.com>
2018-04-27 03:35:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/article-one',function(req,res)    {
    res.sendFile(path.join(__dirname, 'ui', 'rjs1.html'));
 });
